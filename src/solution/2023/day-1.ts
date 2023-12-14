eb1e4d0f8d09092b682c3a4ec760d9feaa690d53
Bagus <bnanda@cleanmedicindus.com>
2023-12-15 00:50:08 +0800

    remove console log

--- src/solution/2023/day-1.ts
+++ src/solution/2023/day-1.ts
@@ -2,11 +2,6 @@ import { first_num, last_num } from "@lib/number"
 import { read_file } from "@lib/utils"
 
 export async function sum_of_calibration() {
-  const file = await read_file()
-  console.log(file);
-  
+  const file = await read_file()  
   return file.reduce((acc, cur) => acc + parseInt(`${first_num(cur)}${last_num(cur)}`), 0)
 }
-
-console.log(await sum_of_calibration());
-
