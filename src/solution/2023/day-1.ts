cf32fc252f38bf001ad8e7c6fd549025edd5a3f5
Bagus <bnanda@cleanmedicindus.com>
2023-12-15 00:48:20 +0800

    update day 1 solution $ add import map

--- src/solution/2023/day-1.ts
+++ src/solution/2023/day-1.ts
@@ -1,33 +1,12 @@
-import { read_file } from "../../../utils"
+import { first_num, last_num } from "@lib/number"
+import { read_file } from "@lib/utils"
 
-const calibration = await read_file("day-1")
-
-const regex = /one|two|three|four|five|six|seven|eight|nine|[0-9]/
-
-const spelled_digit: Record<string, number> = {
-  zero: 0,
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6,
-  seven: 7,
-  eight: 8,
-  nine: 9
+export async function sum_of_calibration() {
+  const file = await read_file()
+  console.log(file);
+  
+  return file.reduce((acc, cur) => acc + parseInt(`${first_num(cur)}${last_num(cur)}`), 0)
 }
 
-const to_number = (s: string) => spelled_digit[s] ?? parseInt(s)
+console.log(await sum_of_calibration());
 
-const first_number = (s: string) => s.match(regex)?.map(to_number)
-
-const last_number = (s: string) => s.match(new RegExp(regex.source + '$'))?.map(to_number)
-
-const sum = calibration
-  .reduce((acc, cur) => {
-    const first = first_number(cur)
-    const last = last_number(cur)
-    return acc += parseInt(`${first}${last}`)
-  }, 0)
-
-console.log(sum);
