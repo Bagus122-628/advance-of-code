d87d1d2bd940a334d9f1f06b0a71ca32ce513ab8
Bagus <bnanda@cleanmedicindus.com>
2023-12-15 22:50:45 +0800

    fix error file not found

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,11 +5,11 @@ export async function read_file() {
 
 export function current_path() {
     const stack = new Error()?.stack?.split('\n');
-    const stack_path = stack?.at(-1)?.match(/\(([^)]+)\)/);
+    const stack_path = stack?.at(-2)?.match(/\(([^)]+)\)/);
     const file_path = stack_path?.[1]?.split(':')[0]
     const file = file_path?.replace(/.ts|.test.ts/, '.txt')
 
-    if (file) return file
+    if (!file) throw new Error('file not found')
 
-    throw new Error('file not found')
+    return file
 }
