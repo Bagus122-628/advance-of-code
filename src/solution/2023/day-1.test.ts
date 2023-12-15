29b9e2dd3b6799c64d618d20e594023de628c783
Bagus <bnanda@cleanmedicindus.com>
2023-12-15 15:11:10 +0800

    mv day 1 solution to day 1 test

--- src/solution/2023/day-1.test.ts
+++ src/solution/2023/day-1.test.ts
@@ -1,6 +1,17 @@
 import { test, expect, describe } from 'bun:test';
-import { sum_of_calibration } from './day-1';
+import { first_num, last_num } from "@lib/number"
+import { read_file } from "@lib/utils"
+
+test('2023/day-1.part-1', async () => {
+    expect(await sum_of_calibration()).toEqual(281);
+});
 
 test('2023/day-1.part-2', async () => {
     expect(await sum_of_calibration()).toEqual(281);
 });
+
+const calibration = await read_file()
+
+async function sum_of_calibration(file: string[] = calibration) {
+    return file.reduce((acc, cur) => acc + parseInt(`${first_num(cur)}${last_num(cur)}`), 0)
+}
--- src/solution/2023/day-1.ts
+++
@@ -1,7 +0,0 @@
-import { first_num, last_num } from "@lib/number"
-import { read_file } from "@lib/utils"
-
-export async function sum_of_calibration() {
-  const file = await read_file()  
-  return file.reduce((acc, cur) => acc + parseInt(`${first_num(cur)}${last_num(cur)}`), 0)
-}
