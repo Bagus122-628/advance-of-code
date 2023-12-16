d82b9e89ad4b1af1ae6fc5efd8310db5f02ffcac
Bagus <bnanda@cleanmedicindus.com>
2023-12-17 00:41:49 +0800

    add day 4 part 2 solution

--- src/solution/2023/day-4.test.ts
+++ src/solution/2023/day-4.test.ts
@@ -1,4 +1,5 @@
 import { test, expect } from 'bun:test';
+import puzzle from "./puzzle.txt";
 
 const input = `
 Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -9,14 +10,38 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 `
 test('2023/day-4/part-1', () => expect(scratchcards_point(input)).toEqual(13))
+test('2023/day-4/part-2', () => expect(total_scratchcards(input)).toEqual(30))
 
 function scratchcards_point(input: string): number {
     return input.trim().split('\n').reduce((sum, card) => {
-        const [g, wn, yn] = card.split(/: |\| /)
-        let point = matching(wn, yn)
+        const [_, wn, yn] = card.split(/: |\| /)
+        const point = matching(wn, yn)
 
         return !point ? sum : sum + parseInt(point, 2)
     }, 0)
+}
+
+function total_scratchcards(input: string): number {
+    const line = input.trim().split('\n')
+    const instances = Array.from({ length: line.length }, () => 1);
+
+    for (const card of line) {
+        const [g, wn, yn] = card.split(/: |\| /)
+        const cn = parseInt(g.split(/\s+/).at(1) ?? '');
+        const point = matching(wn, yn)
+
+        update_instance(point, cn, instances);
+    }
+    return instances.reduce((total, instance) => total + instance, 0);
+
+}
+
+function update_instance(point: string, cn: number, instances: number[]) {
+    for (let i = 0; i < point.length; i++) {
+        if (cn + i < instances.length) {
+            instances[cn + i] = instances[cn + i] + instances[cn - 1];
+        }
+    }
 }
 
 function matching(wining_card: string, your_card: string) {
